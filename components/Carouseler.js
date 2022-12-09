6263ff06e7ca2d8fd3ebaad21e8add71dc108ca6
AbdeldjabarDev <91795096+AbdeldjabarDev@users.noreply.github.com>
2022-12-09 09:39:11 +0100

    Update Carouseler.js

--- components/Carouseler.js
+++ components/Carouseler.js
@@ -3,7 +3,9 @@ export default function Carouseler(props)
 {
     let conRef = useRef();
     let [itemIndex,setItemIndex] = useState(0);    
-    if(props.controls)
+     children= [<Carousel />,<Carousel />]
+   controls = [<div className='w-10 h-10 bg-[url("/images/left-icon.png")] bg-pink-600 bg-no-repeat bg-center rounded-full'></div>,<div className='w-10 h-10 bg-[url("/images/right-icon.png")] bg-black bg-no-repeat bg-center rounded-full'></div>]
+    if(controls)
     {
       return(<div className={" " + props.tailwind} >
       <div className="flex flex-col lg:flex-row">
@@ -11,23 +13,23 @@ export default function Carouseler(props)
      
       </div>
       <div className="flex flex-col w-full h-full overflow-hidden" >
-      <div className="flex h-full "  ref={conRef} style={{width:props.children.length*100 +"%",transition:"transform .5s ease-out",transform:`translateX(-${itemIndex/props.children.length*100}%)`}}>
-        {props.children}
+      <div className="flex h-full "  ref={conRef} style={{width:children.length*100 +"%",transition:"transform .5s ease-out",transform:`translateX(-${itemIndex/children.length*100}%)`}}>
+        {children}
       </div>
      <div className="flex gap-4"> <div className="z-20" onClick={(e)=>
     {console.log("control clicked : right");
     if(itemIndex < 0)
     {setItemIndex(0);return};
-    if(itemIndex == props.children.length -1 )
+    if(itemIndex == children.length -1 )
     {return} 
-     setItemIndex(itemIndex+1)}}>{props.controls[0]}</div>
+     setItemIndex(itemIndex+1)}}>{controls[0]}</div>
 <div className="z-20" onClick={(e)=>{
     console.log("control clicked : left ");
     if(itemIndex < 0)
     {setItemIndex(0);return};
-    setItemIndex(itemIndex-1)}}>{props.controls[1]}</div></div>
+    setItemIndex(itemIndex-1)}}>{controls[1]}</div></div>
       </div>
-      {/* <div className="absolute font-bold text-xl z-30" style={{top:"3%",left:"4%"}}>{itemIndex}</div> */}
+  
       </div>
       
       )  
@@ -35,13 +37,13 @@ export default function Carouseler(props)
     return (
         <div className={" " + props.tailwind}>
         <div className="flex flex-col lg:flex-row" >
-        <div className="flex  h-full "  ref={conRef} style={{width:props.children.length*100 +"%",transition:"transform .5s ease-out",transform:`translateX(-${itemIndex/props.children.length*100}%)`}}>
-            {props.children}
+        <div className="flex  h-full "  ref={conRef} style={{width:children.length*100 +"%",transition:"transform .5s ease-out",transform:`translateX(-${itemIndex/children.length*100}%)`}}>
+            {children}
         </div>
         <div className="flex gap-4">
         
         <div className="p-4 bg-red-300"     onClick={(e)=>{if(itemIndex < 1) {return}setItemIndex(itemIndex-1) }}>{"<"}</div>
-           <div className="p-4 bg-blue-300" onClick={(e)=>{if(itemIndex == props.children.length -1) {return}setItemIndex(itemIndex+1) }}> {">"}</div>
+           <div className="p-4 bg-blue-300" onClick={(e)=>{if(itemIndex == children.length -1) {return}setItemIndex(itemIndex+1) }}> {">"}</div>
           
         </div>
         </div>
